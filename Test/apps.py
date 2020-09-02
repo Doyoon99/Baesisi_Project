39887f91fa903114e420990c9867b024c4fcb069
Doyoon <qkrehdbs1106@naver.com>
2020-09-02 21:47:52 +0900

    heroku edited

--- Test/apps.py
+++ Test/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class TestConfig(AppConfig):
     name = 'Test'
+
+app.listen(process.env.PORT or 5000, function(){
+
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+})
